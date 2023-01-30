e0eb0aa5c02ebe4a871e51671ca1ba42d938d4ba
Chris Kark <ckark@alumni.stanford.edu>
2023-01-30 09:39:28 -0500

    Automatically removes auto-layout on parent frames and component sets

--- code.js
+++ code.js
@@ -17,7 +17,7 @@ figma.parameters.on('input', ({ query, key, result }) => {
             validateInput(query, result, ['Auto', '1', '2', '4', '6', '8', '12', '14', '16']);
             break;
         case 'gap':
-            validateInput(query, result, ['4', '8', '16', '24', '32', '40', '48', '56']);
+            validateInput(query, result, ['0', '2', '4', '8', '16', '24', '32', '40', '48', '56']);
             break;
         case 'sort':
             validateInput(query, result, ['No', 'Ascending', 'Descending']);
@@ -27,20 +27,24 @@ figma.parameters.on('input', ({ query, key, result }) => {
     }
 });
 figma.on('run', ({ parameters }) => {
-    const grid = (e, t, n, a) => {
-        t = !0 === isNaN(t) ? e.length : parseInt(t);
-        let o = [...figma.currentPage.selection].sort((e, t) => e.x - t.x).sort((e, t) => e.y - t.y);
-        'No' === a ? (e = o) : 'Descending' === a ? (e = o.sort((e, t) => e.name.localeCompare(t.name))) : 'Ascending' === a && (e = o.sort((e, t) => t.name.localeCompare(e.name)));
-        let r = 1 / 0, l = 1 / 0;
-        for (const t of e)
-            (r = Math.min(r, t.x)), (l = Math.min(l, t.y));
-        let s = r, i = l, m = 0;
-        for (let a = 0; a < e.length; a++) {
-            const o = e[a].width, l = e[a].height, c = e[a];
-            (c.x = s), (c.y = i), (m = Math.max(m, l)), (s += o + n), (a + 1) % t == 0 && ((s = r), (i += m + n), (m = 0));
-        }
-    };
-    0 === figma.currentPage.selection.length
-        ? figma.closePlugin('Please select at least one element.')
-        : (grid(figma.currentPage.selection, parameters.columns, parseInt(parameters.gap), parameters.sort), figma.closePlugin('Selection griddled. 🧇'));
+    1 >= figma.currentPage.selection.length
+        ? figma.closePlugin('Please select at least two elements.')
+        : (figma.currentPage.selection.map((e) => {
+            ('FRAME' !== e.parent.type && 'COMPONENT_SET' !== e.parent.type) || (e.parent.layoutMode = 'NONE');
+        }),
+            ((e, t, a, n) => {
+                t = !0 === isNaN(t) ? e.length : parseInt(t);
+                let r = [...figma.currentPage.selection].sort((e, t) => e.x - t.x).sort((e, t) => e.y - t.y);
+                'No' === n ? (e = r) : 'Descending' === n ? (e = r.sort((e, t) => e.name.localeCompare(t.name))) : 'Ascending' === n && (e = r.sort((e, t) => t.name.localeCompare(e.name)));
+                let l = 1 / 0, o = 1 / 0;
+                for (const t of e)
+                    (l = Math.min(l, t.x)), (o = Math.min(o, t.y));
+                let s = l, c = o, i = 0;
+                for (let n = 0; n < e.length; n++) {
+                    const r = e[n].width, o = e[n].height, m = e[n];
+                    (m.x = s), (m.y = c), (i = Math.max(i, o)), (s += r + a), (n + 1) % t == 0 && ((s = l), (c += i + a), (i = 0));
+                }
+            })(figma.currentPage.selection, parameters.columns, parseInt(parameters.gap), parameters.sort),
+            console.clear(),
+            figma.closePlugin('Selection griddled. 🧇'));
 });
